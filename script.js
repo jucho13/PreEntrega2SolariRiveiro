79b0ac3bf6c776b7fb795c12a642cd55f010b700
Jucho <juliansolaririveiro@yahoo.com>
2023-02-15 23:30:55 -0300

    Casi listo

---
+++ script.js
@@ -0,0 +1,42 @@
+class producto
+{
+    constructor(nombre,precio)
+    {
+        this.nombre=nombre;
+        this.precio=precio;
+    }
+    sumaIva()
+    {
+        return this.precio * 1.21;
+    }
+}
+const fideos = new producto("Fideos", 209);
+const leche = new producto("Leche", 240);
+const yogurt = new producto("Yogurt", 340);
+const tapaDeTarta = new producto("Tapa de Tarta", 190);
+const cocaCola = new producto("Coca Cola", 210);
+const papasFritas = new producto("Papas Fritas", 170);
+const mani = new producto("Mani", 90);
+const pan = new producto("Pan", 200);
+
+let productos=[fideos,leche,yogurt,tapaDeTarta,cocaCola,papasFritas,mani,pan];
+alert("Buenas, bienvenido a mi simulador virtual de Despensa");
+alert("Nuestros precios sin iva son los siguientes:");
+for(i=0;i<8;i++)
+{
+    alert(`${productos[i].nombre} tiene un valor de ${productos[i].precio}`);
+}
+let sigue="si";
+while (sigue=="si")
+{
+    let productoComprado=prompt("¿Que producto le gustaria comprar?");
+    let cantidadProducto=parseInt(prompt("¿Cuantos les gustaria comprar?"));
+    if(productoComprado==productos.nombre)
+    {
+        let subTotal=(productos.sumaIva()*cantidadProducto);
+    }
+    let total=total+subTotal;
+    sigue=prompt("¿Le gustaria seguir comprando?");
+}
+
+alert(`El total de su compra es ${total} pesos, ¡Gracias por su compra!`);
